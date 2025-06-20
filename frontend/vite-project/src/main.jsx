5ccd9dcacbe35cdd3a79fe4c7012d9bed9b0fea8
vinodhini-kalaichelvan <vinodhinispec@gmail.com>
2025-06-20 02:04:52 -0300

    activity_2

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
      <App />
+     <ToastContainer />
   </Provider>
 );
--- frontend/vite-project/src/redux/actions/productActions.js
+++ frontend/vite-project/src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-import { toast } from "react-bootstrap";
+import { toast } from "react-toastify";
 import API_URL from "../../api";
 
  //fetch
